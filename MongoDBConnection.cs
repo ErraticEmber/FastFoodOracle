8aaacc1db8d1a6bc392d03f6abd8c24deaecbb6f
Matthew Burzinski <Matthewburzinski@gmail.com>
2021-05-31 15:45:33 -0500

    Simple MongoDB connection

---
+++ MongoDBConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace FastFoodOracle
+{
+    class MongoDBConnection
+    {
+        private readonly MongoClient dbClient;
+        public MongoDBConnection()
+        {
+            dbClient = new MongoClient("mongodb+srv://application:ox51afLsGCd071iB@fastfoodoraclecluster.2qlbx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        }
+
+        public void printAllDBs()
+        {
+            var dbList = dbClient.ListDatabases().ToList();
+            foreach(var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace FastFoodOracle
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Fast Food Oracle!");
+            MongoDBConnection connection = new MongoDBConnection();
+            connection.printAllDBs();
         }
     }
 }
