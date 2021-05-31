0889bf5ab7d090d3618b07ee68a186cd2e08008d
Matthew Burzinski <Matthewburzinski@gmail.com>
2021-05-31 10:31:02 -0500

    Initial Commit for Console App

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastFoodOracle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Fast Food Oracle!");
+        }
+    }
+}
